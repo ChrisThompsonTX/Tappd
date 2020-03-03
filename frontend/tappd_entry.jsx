fb379939dd6f80848720c48ba24077b22e684845
Chris Thompson <chris.tx86@gmail.com>
2020-03-03 10:26:24 -0800

    add reducer and actions for session

--- frontend/tappd_entry.jsx
+++ frontend/tappd_entry.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {login, signup, logout} from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
+    
+    window.login = login;
+    window.singup = signup;
+    window.logout = logout;
+
+
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Tappd</h1>, root);
 });
